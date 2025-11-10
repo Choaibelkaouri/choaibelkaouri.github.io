64db9ac71facee362b0d0804243dd12373ee7f3e
CHOAIB EL KAOURI <65832846+Choaibelkaouri@users.noreply.github.com>
2025-11-10 13:50:47 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root") as HTMLElement;
+
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
